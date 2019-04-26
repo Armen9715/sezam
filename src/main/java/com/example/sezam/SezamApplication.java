bda43c9657a9b9f581bb691d744e4d61c1cddf2c
armen27gh <48012934+armen27gh@users.noreply.github.com>
2019-04-26 23:04:53 +0400

    new project start

---
+++ src/main/java/com/example/sezam/SezamApplication.java
@@ -0,0 +1,13 @@
+package com.example.sezam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SezamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SezamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sezam/SezamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sezam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SezamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
